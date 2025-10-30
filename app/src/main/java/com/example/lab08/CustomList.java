ef41c267022519545ebc73031e778ecada1c4c8f
Shula Juguilon <sjuguilo@ualberta.ca>
2025-10-30 17:38:21 -0600

    Implementing hasCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -13,8 +13,9 @@ public class CustomList {
     public void addCity(City city) {
         cities.add(city);
     }
-
-    
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
